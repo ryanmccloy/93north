79d70d9d77ef01a60c36bada83703f143fb38de2
Ryan McCloy <ryannmccloy@gmail.com>
2025-08-03 21:18:52 +0100

    playing with styling in service containers

--- app/_components/reusable/ServiceContainer.js
+++ app/_components/reusable/ServiceContainer.js
@@ -1,47 +1,57 @@
 import ClickIcon from "../icons/ClickIcon";
 import DoubleArrowDown from "../icons/DoubleArrowDown";
 
-function ServiceContainer({ service, isOpen, handleClick, isLast }) {
+function ServiceContainer({ title, content, isOpen, handleClick, isLast }) {
   return (
     <div
-      className={`relative group border-t ${
+      className={` relative group border-t ${
         isLast ? "border-b" : ""
       } border-t-black py-30 cursor-pointer overflow-hidden hover:text-white ${
-        isOpen ? "bg-black text-white" : ""
+        isOpen ? "bg-black text-white service-background" : ""
       }`}
       onClick={handleClick}
     >
       {/* Background animation layer */}
-      <div className="absolute top-0 left-0 h-full w-0 bg-black z-0 group-hover:w-full transition-all duration-500 ease-out" />
+      <div
+        className={`service-background absolute top-0 left-0 h-full w-0 bg-black z-0 group-hover:w-full transition-all duration-500 ease-out ${
+          isOpen ? "w-full" : ""
+        }`}
+      />
 
       {/* Foreground content */}
       <div className={`relative z-10 width-size `}>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-30">
+        <div className="flex gap-30 items-center">
+          <div className="flex  items-center gap-30">
             <DoubleArrowDown />
             <h3 className="uppercase text-heading-medium font-extralight  ">
-              {service}
+              {title}
             </h3>
           </div>
           <div
-            className={`transition-all text-white duration-500 ease-in-out flex gap-30 items-center ${
+            className={`transition-all mt-15 text-white duration-500 ease-in-out flex gap-30 items-center ${
               isOpen
                 ? "opacity-0 translate-y-2 pointer-events-none"
                 : "opacity-100 translate-y-0"
             }`}
           >
-            <span>click to expand</span>
-            <ClickIcon />
+            <span className="">click to expand</span>
+            {/* <ClickIcon /> */}
           </div>
         </div>
 
         {/* Expandable content */}
         <div
-          className={` bg-red-200 w-full overflow-hidden transition-all duration-500 ease-in-out ${
-            isOpen ? "mt-30 h-[300px]" : "h-0"
+          className={` w-full overflow-hidden transition-all duration-500 ease-in-out flex flex-col gap-30 ${
+            isOpen ? "mt-30 max-h-[1000px] pb-15" : "max-h-0"
           }`}
         >
-          {/* Your expandable content goes here */}
+          {content.map((paragraph, index) => {
+            return (
+              <p key={index} className="max-w-[700px]">
+                {paragraph}
+              </p>
+            );
+          })}
         </div>
       </div>
     </div>
